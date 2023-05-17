725a07a96a5077206a8e3e9df686c1eb7bedf924
TomVdt <tomtommy135@gmail.com>
2023-05-17 22:11:41 +0200

    clean up unused constants

--- src/include/constants.h
+++ src/include/constants.h
@@ -9,11 +9,4 @@ namespace CONSTANTS {
     inline constexpr Vector3D g(0.0, -9.81, 0.0);
     inline constexpr double PHYSICS_DT(0.01);
     inline constexpr double PHYSICS_DPOS(0.1);
-
-    // Camera
-    // 60deg
-    inline constexpr double CAMERA_FOV(1.047197551);
-    inline constexpr double CAMERA_RATIO(1.0);
-    inline constexpr double CAMERA_NEAR(0.1);
-    inline constexpr double CAMERA_FAR(1000.0);
 };
